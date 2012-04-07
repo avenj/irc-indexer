77ea101afe9e85cb39b2335189b051b05fab0c14
avenj <avenj@cobaltirc.org>
2012-04-07 16:48:24 -0400

    POD for IRC::Indexer stub
    	modified:   lib/IRC/Indexer.pm

--- lib/IRC/Indexer.pm
+++ lib/IRC/Indexer.pm
@@ -1,9 +1,34 @@
 package IRC::Indexer;
 our $VERSION = '0.01';
 
+## stub! for now ...
+
 1;
 __END__
 
 =pod
+
+=head1 NAME
+
+IRC::Indexer - IRC server indexing via POE
+
+=head1 SYNOPSIS
+
+FIXME
+
+=head1 DESCRIPTION
+
+A set of modules and utilities useful for trawling IRC networks, 
+collecting information, and exporting it to portable formats for use in 
+Web frontends and other applications.
+
+FIXME frontends
+
+See the B<perldoc> for L<IRC::Indexer::Trawl::Bot> for more about 
+using the trawl bot itself as part of other POE-enabled applications.
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
 
 =cut
