90254deac27936a589f656f03e79cdef7e9366bb
avenj <avenj@cobaltirc.org>
2012-04-07 15:45:23 -0400

    Working run.
    	modified:   lib/IRC/Indexer/Trawl/Bot.pm

--- lib/IRC/Indexer/Trawl/Bot.pm
+++ lib/IRC/Indexer/Trawl/Bot.pm
@@ -23,6 +23,8 @@ sub new {
   my $self = {};
   bless $self, $class;
 
+  $self->{State} = {};
+
   ## an outline of NetInfo just for ease of reference
   $self->{NetInfo} = {
     ## Status:
@@ -41,8 +43,8 @@ sub new {
     GlobalUsers => undef,  # lusers
     OperCount   => undef,  # lusers
     ListLinks   => undef,  # available link list
-    ListChans   => undef,  # available chan list    
-    HashChans   => undef,  # available chan hash
+    ListChans   => [],     # available chan list    
+    HashChans   => {},     # available chan hash
     MOTD        => undef,  # MOTD as array
     
     StartedAt   => time(),
@@ -54,7 +56,8 @@ sub new {
   $args{lc $_} = delete $args{$_} for keys %args;
 
   $self->{timeout}   = $args{timeout}  || 120;
-  $self->{interval}  = $args{interval} || 20;
+  $self->{interval}  = $args{interval} || 10;
+  $self->{verbose}   = $args{verbose}  || 0;
 
   $self->{ircserver} = $args{server} ? $args{server} 
                              : croak "No Server specified in new" ;
@@ -88,6 +91,7 @@ sub new {
          'irc_376',
         ## LINKS
          'irc_364',
+         'irc_365',
         ## LUSERS
          'irc_251',
          'irc_252',
@@ -224,16 +228,21 @@ sub _retrieve_info {
   ##  - LIST
   ## stagger them out at reasonable intervals to avoid flood prot:
   my $alrm = 2;
-  for my $cmd (qw/lusers list links/) {
-    $kernel->alarm('_issue_cmd', time + $alrm, $cmd);
+  for my $cmd (qw/list links lusers/) {
+    $kernel->alarm_add('_issue_cmd', time + $alrm, $cmd);
     $alrm += $self->{interval};
   }
 }
 
 sub _issue_cmd {
-  my ($self, $type) = @_[OBJECT, ARG0];
+  my ($self, $cmd) = @_[OBJECT, ARG0];
   my $irc = $self->{ircobj};
-  $irc->yield($type);
+  
+  ## most servers will announce lusers at connect-time:
+  return if $cmd eq 'lusers' and $self->{State}->{Lusers};
+  
+  warn "-> issuing $cmd\n" if $self->{verbose};
+  $irc->yield($cmd);
 }
 
 sub _check_timeout {
@@ -241,12 +250,27 @@ sub _check_timeout {
   my $irc = $self->{ircobj};
   my $info = $self->netinfo;
   
+  my $shutdown = 0;
+  
+  my @states = qw/Links Lusers MOTD List/;
+  my $stc = 0;
+  for my $state (@states) {
+    next unless $self->{State}->{$state};
+    $stc++;
+    warn "-> have state: $state\n" if $self->{verbose};
+  }
+  
+  $shutdown++ if $stc == scalar @states;
+
   my $startedat = $info->{StartedAt} || return;
   
-  if (time - $startedat > $self->{timeout}) {
+  $shutdown++ if time - $startedat > $self->{timeout};
+
+  if ($shutdown) {
+    warn "-> shutdown\n" if $self->{verbose};
     $self->done(1);
     $irc->yield('disconnect');
-    $irc->yield('shutdown');
+    $irc->yield('shutdown');  
   }
   
   $kernel->alarm( '_check_timeout', time + 10 );
@@ -286,7 +310,7 @@ sub irc_001 {
   my ($self, $kernel, $heap) = @_[OBJECT, KERNEL, HEAP];
   my $irc = $self->{ircobj};
   ## let things settle out, then _retrieve_info:
-  $kernel->alarm('_retrieve_info', time + 8);
+  $kernel->alarm('_retrieve_info', time + 6);
 }
 
 sub irc_375 {
@@ -308,6 +332,8 @@ sub irc_376 {
   my ($self) = $_[OBJECT];
   my $info = $self->netinfo;
   push(@{ $info->{MOTD} }, "End of MOTD.");
+  
+  $self->{State}->{MOTD} = 1;
 }
 
 sub irc_364 {
@@ -320,9 +346,17 @@ sub irc_364 {
   push(@{ $info->{ListLinks} }, $_[ARG1]);
 }
 
+sub irc_365 {
+  ## end of LINKS
+  $_[OBJECT]->{State}->{Links} = 1;
+}
+
 sub irc_251 {
   my ($self) = $_[OBJECT];
   my $info = $self->netinfo;
+  
+  $self->{State}->{Lusers} = 1;
+  
   my $rawline;
   ## LUSERS
   ## may require some fuckery ...
@@ -354,12 +388,13 @@ sub irc_322 {
   ## LIST
   my ($self) = $_[OBJECT];
   my $info = $self->netinfo;
-  my $split;
-  return unless $split = $_[ARG2];
+  my $split = $_[ARG2] // return;
   my ($chan, $users, $topic) = @$split;
-  return unless $chan;
+
+  warn "chan -> $chan $users $topic\n" if $self->{verbose};
+
   $users //= 0;
-  $topic //= '';
+  $topic //= ''; 
   
   ## Add a hash element
   ## _323 triggers a ListChans rebuild, below
@@ -373,6 +408,9 @@ sub irc_323 {
   ## LIST ended
   ## sorted our hash into ListChans
   my ($self) = $_[OBJECT];
+  
+  $self->{State}->{List} = 1;
+  
   my $info = $self->netinfo;
   my $chash = $info->{HashChans};
   return unless keys %$chash;
