91c8a620b19af8178681512292a9d6b586fb664c
avenj <avenj@cobaltirc.org>
2012-04-08 00:24:51 -0400

    Info::Network; add_server()
    	modified:   lib/IRC/Indexer/Info/Network.pm

--- lib/IRC/Indexer/Info/Network.pm
+++ lib/IRC/Indexer/Info/Network.pm
@@ -10,7 +10,21 @@ sub new {
   my $self = {},
   my $class = shift;
   bless $self, $class;
-  $self->{Network} = {};
+  $self->{Network} = {
+    Servers => {
+     ## ServerName => {
+     ##   MOTD => [],
+     ## }
+    },
+    
+    OperCount   => undef,
+    GlobalUsers => undef,
+    ListChans   => [],
+    HashChans   => {},
+    
+    ConnectedAt => undef,
+    FinishedAt  => undef,
+  };
   return $self
 }
 
@@ -22,9 +36,22 @@ sub add_server {
   my ($self, $info) = @_;
   ## given a Info::Server object, merge to this Network
   return unless $info and ref $info;
-  my $network = $info->network;
+  my $name = $info->server;
+  my $motd = $info->motd;  
   
-  ## FIXME
+  ## keyed on reported server name
+  ## will "break"-ish on dumb nets announcing dumb names:
+  my $network = $self->{Network};
+  my $servers = $network->{Servers};
+  $servers->{$name}->{MOTD} = $motd;
+  
+  ## these can all be overriden network-wide:
+  $network->{GlobalUsers} = $info->users;
+  $network->{OperCount}   = $info->opers;
+  $network->{ListChans}   = $info->channels;
+  $network->{HashChans}   = $info->chanhash;
+  $network->{ConnectedAt} = $info->connectedat;
+  $network->{FinishedAt}  = $info->finishedat;
 }
 
 1;
