d9a73aa3ce5a52d02a7fc2c161828c50e015f2e2
avenj <avenj@cobaltirc.org>
2012-04-08 00:38:59 -0400

    Info::Network; POD
    	modified:   lib/IRC/Indexer/Info/Network.pm

--- lib/IRC/Indexer/Info/Network.pm
+++ lib/IRC/Indexer/Info/Network.pm
@@ -40,6 +40,13 @@ sub servers {
   return $self->{Network}->{Servers}
 }
 
+sub motd_for {
+  my ($self, $server) = @_;
+  return unless $server;
+  return unless exists $self->{Network}->{Servers}->{$server};
+  return $self->{Network}->{Servers}->{$server}->{MOTD} // []
+}
+
 sub opers {
   my ($self) = @_;
   return $self->{Network}->{OperCount}
@@ -94,3 +101,79 @@ sub add_server {
 
 1;
 __END__
+
+=pod
+
+=head1 NAME
+
+IRC::Indexer::Info::Network - Network information class for IRC::Indexer
+
+=head1 SYNOPSIS
+
+  my $network = IRC::Indexer::Info::Network->new;
+  ## Get ::Info::Server object from finished trawl bot:
+  my $servinf  = $trawler->info;
+  ## Feed it to add_server:
+  $network->add_server( $servinf );
+  ## Get a network info hash:
+  my $ref = $network->dump;
+
+=head1 DESCRIPTION
+
+This is a simple Network class for L<IRC::Indexer>, providing an easy 
+way to merge multiple trawled servers into a single network summary.
+
+=head2 METHODS
+
+=head3 add_server
+
+Merges server information from a Trawl::Bot run.
+
+Argument must be a L<IRC::Indexer::Info::Server> object.
+
+=head3 dump
+
+Returns the network information hash.
+
+=head3 connectedat
+
+Returns the connect timestamp of the last run for this network.
+
+=head3 finishedat
+
+Returns the timestamp of the last run for this network.
+
+=head3 servers
+
+Returns a hash keyed on server name.
+
+=head3 motd_for
+
+Returns the MOTD for a specified server:
+
+  my $motd = $network->motd_for($servername);
+
+=head3 users
+
+Returns the global user count if available via B<LUSERS>
+
+=head3 opers
+
+Returns the global operator count if available via B<LUSERS>
+
+=head3 channels
+
+Returns the sorted array of parsed B<LIST> results, as described in 
+L<IRC::Indexer::Trawl::Bot>
+
+=head3 chanhash
+
+Returns the hash containing parsed B<LIST> results, as described in 
+L<IRC::Indexer::Trawl::Bot>
+
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+=cut
