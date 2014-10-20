9744063c698a436604fcba656b70468607939ff0
Jon Portnoy <avenj@cobaltirc.org>
2014-10-19 21:57:11 -0400

    add Role::CmdDispatch

---
+++ lib/IRC/Indexer/Role/CmdDispatch.pm
@@ -0,0 +1,59 @@
+package IRC::Indexer::Role::CmdDispatch;
+
+use strictures 1;
+
+use Scalar::Util 'reftype';
+
+use POE;
+
+
+use Role::Tiny;
+
+requires 'serialize', 'deserialize';
+
+
+sub dispatch_and_reply {
+  my $self = shift;
+  if ( my $rv = $self->dispatch(@_) ) {
+    my (undef, $parts, $sender) = @_;
+    my (undef, $routes) = $parts->tail;
+    $poe_kernel->post( $sender => send_multipart =>
+      $routes->all, $self->serialize($rv)
+    );
+  }
+   
+}
+
+sub dispatch_cmd {
+  my ($self, $type, $parts, $sender) = @_;
+
+  my $component = ucfirst $type;
+
+  my ($body, $routes) = $parts->tail;
+  unless ($body) {
+    warn "Received empty content from $component ($self)\n";
+    return
+  }
+
+  my $cmdhash = $self->deserialize($body);
+  unless (reftype $cmdhash eq 'HASH') {
+    warn
+      "Expected HASH-type data from $component but got $cmdhash ($self)\n";
+    return
+  }
+
+  my $cmd = lc( delete $cmdhash->{cmd} || '' );
+  unless ($cmd && $cmd =~ /^[A-Za-z0-9_]+$/) {
+    warn "Received invalid command '$cmd' from $component ($self)\n";
+    return
+  }
+  my $meth = '_recv_' . $type . '_cmd_' . $cmd;
+  unless ($self->can($meth)) {
+    warn "Received unknown command '$cmd' from $component ($self)\n";
+    return
+  }
+
+  $self->$meth($cmdhash, $routes, $sender)
+}
+
+1;
