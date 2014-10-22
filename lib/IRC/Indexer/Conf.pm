675a676a43e933daa8faeee4a7139a996f0bba90
Jon Portnoy <avenj@cobaltirc.org>
2014-10-21 21:25:56 -0400

    POD

--- lib/IRC/Indexer/Conf.pm
+++ lib/IRC/Indexer/Conf.pm
@@ -34,3 +34,43 @@ sub _coerce {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+IRC::Indexer::Conf - Inflated ZPL configs for IRC::Indexer
+
+=head1 SYNOPSIS
+
+  use IRC::Indexer::Conf;
+
+  my $cfg = IRC::Indexer::Conf->new_from_file(
+    '/my/config.zpl'
+  );
+
+  # Hashes are List::Objects::WithUtils::Hash::Inflated objects:
+  my $bar = $cfg->foo->bar;
+
+  # Lists are List::Objects::WithUtils::Array objects:
+  my @things = $cfg->things->all;
+
+=head1 DESCRIPTION
+
+Reads and inflates C<ZPL> format configuration files via L<Text::ZPL> and
+L<List::Objects::WithUtils>.
+
+=head2 new_from_file
+
+Takes a path to a C<ZPL>-format configuration file and returns a
+L<List::Objects::WithUtils::Hash::Inflated>.
+
+The hash is deeply coerced; hashes are inflated as stated above, and lists
+become L<List::Objects::WithUtils::Array> objects.
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+
+=cut
