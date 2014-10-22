9f5a76769b1e43f954f12cdbd6f3b3b5284ff74a
Jon Portnoy <avenj@cobaltirc.org>
2014-10-21 21:11:00 -0400

    Conf; deep-recursive coercions

--- lib/IRC/Indexer/Conf.pm
+++ lib/IRC/Indexer/Conf.pm
@@ -2,7 +2,12 @@ package IRC::Indexer::Conf;
 
 use strictures 1;
 
+use Carp;
+use Scalar::Util 'reftype';
+
 use List::Objects::WithUtils ();
+require List::Objects::WithUtils::Hash;
+require List::Objects::WithUtils::Array;
 use Path::Tiny ();
 use Text::ZPL ();
 
@@ -11,15 +16,21 @@ use parent 'List::Objects::WithUtils::Hash::Inflated';
 sub new_from_file {
   my ($class, $file) = @_;
   my $zpl = Path::Tiny::path($file)->slurp;
-  my @items = %{ Text::ZPL::decode_zpl($zpl) };
-  my @handled;
-  while (my ($k, $v) = splice @items, 0, 2) {
-    push @handled,
-      ref $v eq 'HASH' ? ($k, $class->new(%$v))
-        : ref $v eq 'ARRAY' ? ($k, List::Objects::WithUtils::Array->new(@$v))
-        : ($k, $v) 
-  }
-  $class->new(@handled)
+  my $ref = Text::ZPL::decode_zpl($zpl);
+  _coerce($ref);
+  $class->new(%$ref)
+}
+
+sub _coerce {
+  ref $_[0] eq 'HASH' ? 
+    $_[0] = List::Objects::WithUtils::Hash::Inflated->new(
+      map {; _coerce($_) } %{ $_[0] }
+    )
+  : ref $_[0] eq 'ARRAY' ?
+    $_[0] = List::Objects::WithUtils::Array->new(
+      map {; _coerce($_) } @{ $_[0] }
+    )
+  : $_[0]
 }
 
 1;
